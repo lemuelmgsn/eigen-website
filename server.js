ea2a2f6f345dce83ec5b00ca839b437dcf19cb25
lemuelmgsn <magsinolemuel68@gmail.com>
2024-08-17 00:14:19 +0200

    v7
    POST formulier en route gemaakt. ook pagina gemaakt waar je de opgeslagen outfits kan zien.

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-
 import express from 'express'
-
 import fetchJson from './helpers/fetch-json.js'
 
 const app = express()
 
 app.set('view engine', 'ejs')
-
 app.set('views', './views')
-
 app.use(express.static('public'))
-
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.set('port', process.env.PORT || 8000)
 
@@ -20,18 +15,19 @@ app.listen(app.get('port'), function () {
 })
 
 // VARIABELEN
-
 const apiUrl = 'https://fdnd-agency.directus.app/items/lemuel_kleding'
 
-// ROUTES
+// Een array om de outfits op te slaan
+let savedOutfits = []
 
+// ROUTES
 app.get('/', function(request, response) {
   Promise.all([
       fetchJson(apiUrl + '?filter[type][_eq]=shirts'),
       fetchJson(apiUrl + '?filter[type][_eq]=broeken'),
       fetchJson(apiUrl + '?filter[type][_eq]=schoenen')
   ]).then(([shirts, broeken, schoenen]) => {
-      response.render('homepage', {
+      response.render('homepage2', {
           shirts: shirts.data,
           broeken: broeken.data,
           schoenen: schoenen.data
@@ -42,5 +38,33 @@ app.get('/', function(request, response) {
   });
 });
 
-// 
+// POST-route voor het opslaan van de kledingstukken
+app.post('/save-selection', function(request, response) {
+  const selectedShirtId = request.body.selectedShirt;
+  const selectedBroekId = request.body.selectedBroek;
+  const selectedSchoenId = request.body.selectedSchoen;
 
+  // Haal de details van de geselecteerde items op via de API
+  Promise.all([
+    fetchJson(apiUrl + '/' + selectedShirtId),
+    fetchJson(apiUrl + '/' + selectedBroekId),
+    fetchJson(apiUrl + '/' + selectedSchoenId)
+  ]).then(([selectedShirt, selectedBroek, selectedSchoen]) => {
+    // Maak een outfit object en voeg deze toe aan de array
+    const newOutfit = {
+      shirt: selectedShirt.data,
+      broek: selectedBroek.data,
+      schoen: selectedSchoen.data
+    };
+    
+    savedOutfits.push(newOutfit);
+
+    // Render de selectiepagina met alle opgeslagen outfits
+    response.render('selection', {
+      outfits: savedOutfits
+    });
+  }).catch((error) => {
+    console.error('Error fetching selected items:', error);
+    response.status(500).send('Internal Server Error');
+  });
+});
